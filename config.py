2a871d52cf5b9f4366d1ef0db37789a0b4d01862
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-17 23:32:09 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
-TOKEN = "8443993110:AAH08NXvjcKty2XBHH13ecD8l5giSCD-ZF4"
+BOT_TOKEN = "8443993110:AAH08NXvjcKty2XBHH13ecD8l5giSCD-ZF4"
 
-BOT_USERNAME = "PayydarVpn_robot"
+NEXT_PAY_API_KEY = "اینجا کلید نکست پی خودت رو بزار"
 
-ADMIN_ID = 633464148
-
-PAYMENT_LINKS = {
-    "sub1": "https://example.com/pay1",
-    "sub2": "https://example.com/pay2",
-    "sub3": "https://example.com/pay3"
-}
+CALLBACK_URL = "https://yourdomain.com/verify"
