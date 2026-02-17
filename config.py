fb62ee8a97c72d87baf308efdbdb409d11684e0e
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-17 23:40:32 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 BOT_TOKEN = "8443993110:AAH08NXvjcKty2XBHH13ecD8l5giSCD-ZF4"
 
-NEXT_PAY_API_KEY = "اینجا کلید نکست پی خودت رو بزار"
+NEXT_PAY_API_KEY = "test-nextpay-key"
 
-CALLBACK_URL = "https://yourdomain.com/verify"
+CALLBACK_URL = "https://example.com/verify"
+BOT_USERNAME = "PayydarVpn_robot"
