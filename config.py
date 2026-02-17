c3d61aa9fef1470e853c07557202ff641244e673
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-17 22:34:14 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 TOKEN = "8443993110:AAH08NXvjcKty2XBHH13ecD8l5giSCD-ZF4"
-ADMIN_ID = 633464148
+
 BOT_USERNAME = "PayydarVpn_robot"
+
+ADMIN_ID = 633464148
+
+
+PAYMENT_LINKS = {
+    "sub1": "https://example.com/pay1",
+    "sub2": "https://example.com/pay2",
+    "sub3": "https://example.com/pay3"
+}
