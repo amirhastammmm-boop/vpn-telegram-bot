c3b17b0e4d1c7e74356586e3b05fbf90c16cc105
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-17 23:58:26 +0330

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "8443993110:AAH08NXvjcKty2XBHH13ecD8l5giSCD-ZF4"
-
-NEXT_PAY_API_KEY = "test-nextpay-key"
-
-CALLBACK_URL = "https://example.com/verify"
-BOT_USERNAME = "PayydarVpn_robot"
