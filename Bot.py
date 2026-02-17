778d657ad257befafa3bdf1ed54362cb38bd7ff4
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-17 16:43:32 +0330

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -14,10 +14,12 @@ def main_menu():
     btn3 = KeyboardButton("📚 آموزش")
     btn4 = KeyboardButton("⭐ امتیاز")
     btn5 = KeyboardButton("📞 پشتیبانی")
+    btn6 = KeyboardButton("🏠 منو")
 
     markup.row(btn1, btn2)
     markup.row(btn3, btn4)
     markup.row(btn5)
+    markup.row(btn6)
 
     return markup
 
@@ -31,24 +33,26 @@ def start(message):
     )
 
 
-# دکمه ها
 @bot.message_handler(func=lambda m: True)
 def buttons(message):
 
     if message.text == "💳 خرید اشتراک":
-        bot.send_message(message.chat.id, "بخش خرید اشتراک")
+        bot.send_message(message.chat.id, "بخش خرید اشتراک", reply_markup=main_menu())
 
     elif message.text == "📦 اشتراک های من":
-        bot.send_message(message.chat.id, "لیست اشتراک های شما")
+        bot.send_message(message.chat.id, "لیست اشتراک های شما", reply_markup=main_menu())
 
     elif message.text == "📚 آموزش":
-        bot.send_message(message.chat.id, "آموزش استفاده")
+        bot.send_message(message.chat.id, "آموزش استفاده", reply_markup=main_menu())
 
     elif message.text == "⭐ امتیاز":
-        bot.send_message(message.chat.id, "امتیاز بده ❤️")
+        bot.send_message(message.chat.id, "امتیاز بده ❤️", reply_markup=main_menu())
 
     elif message.text == "📞 پشتیبانی":
-        bot.send_message(message.chat.id, "ارتباط با پشتیبانی")
+        bot.send_message(message.chat.id, "ارتباط با پشتیبانی", reply_markup=main_menu())
+
+    elif message.text == "🏠 منو":
+        bot.send_message(message.chat.id, "بازگشت به منو", reply_markup=main_menu())
 
 
 print("Bot Started...")
