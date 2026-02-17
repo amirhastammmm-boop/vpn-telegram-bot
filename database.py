ef724887bca6042d70a4821e644e2b7e8f2f6543
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-17 21:27:00 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,29 @@
 import sqlite3
 
-conn = sqlite3.connect("users.db", check_same_thread=False)
+conn = sqlite3.connect("vpn.db", check_same_thread=False)
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER,
-    config TEXT
+CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER PRIMARY KEY,
+    subscription TEXT DEFAULT "ندارد",
+    points INTEGER DEFAULT 0
 )
 """)
 
 conn.commit()
+
+
+def add_user(user_id):
+    cursor.execute("INSERT OR IGNORE INTO users(user_id) VALUES (?)", (user_id,))
+    conn.commit()
+
+
+def get_user(user_id):
+    cursor.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
+    return cursor.fetchone()
+
+
+def add_points(user_id, amount):
+    cursor.execute("UPDATE users SET points = points + ? WHERE user_id=?", (amount, user_id))
+    conn.commit()
