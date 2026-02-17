e9564a3e9bc65b5e5ff4442d55ada09c437b20b9
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-17 19:46:37 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 TOKEN = "8443993110:AAH08NXvjcKty2XBHH13ecD8l5giSCD-ZF4"
 
 BOT_USERNAME = "PayydarVpn_robot"
+PAYMENT_LINKS = {
+    "sub1": "https://your-payment-link.com/1month",
+    "sub2": "https://your-payment-link.com/2month",
+    "sub3": "https://your-payment-link.com/3month"
+}
