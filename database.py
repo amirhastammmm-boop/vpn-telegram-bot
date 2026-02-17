4ed7238fa3e76227c7f9858290cfd27a5a482985
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-17 21:11:59 +0330

    Create database.py

---
+++ database.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect("users.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER,
+    config TEXT
+)
+""")
+
+conn.commit()
