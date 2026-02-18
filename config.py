ed30876b6b22e48bd1930c2b03f4f7dfdd41f509
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-19 00:09:31 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,1 @@
 BOT_TOKEN = "8443993110:AAH08NXvjcKty2XBHH13ecD8l5giSCD-ZF4"
-
-ADMIN_ID = "Amnbytry"
-
-NEXT_PAY_LINKS = {
-    "month1": "https://nextpay.org/payment-link-1",
-    "month2": "https://nextpay.org/payment-link-2",
-    "month3": "https://nextpay.org/payment-link-3"
-}
