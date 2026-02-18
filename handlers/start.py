a46a761c9ee754f908e51efaa17a1701a48fd3b7
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-19 00:04:47 +0330

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,22 +1,25 @@
 from telebot import types
-from database import add_user
+from database import create_user
 
 def register_start_handlers(bot):
 
     @bot.message_handler(commands=['start'])
     def start(message):
-
         args = message.text.split()
-        ref_code = args[1] if len(args) > 1 else None
 
-        add_user(message.from_user.id, ref_code)
+        invited_by = None
+        if len(args) > 1:
+            invited_by = int(args[1])
+
+        create_user(message.from_user.id, invited_by)
 
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.row("💳 خرید اشتراک", "📦 اشتراک های من")
-        markup.row("🏆 امتیاز های من")
+        markup.add("💳 خرید اشتراک")
+        markup.add("📦 اشتراک های من")
+        markup.add("🏆 امتیاز های من")
 
         bot.send_message(
             message.chat.id,
-            "سلام دوست عزیز ❤️\nبه ربات خوش اومدی",
+            "خوش اومدی 👋",
             reply_markup=markup
         )
