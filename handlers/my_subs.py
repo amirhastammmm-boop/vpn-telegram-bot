2279343ece87ed4bfb1d54e6afd7c409c5f6b081
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 23:52:45 +0330

    Update my_subs.py

--- handlers/my_subs.py
+++ handlers/my_subs.py
@@ -1,11 +1,18 @@
+from database import get_subs
+
 def register_my_sub_handlers(bot):
 
-    @bot.message_handler(func=lambda m: m.text == "⭐ امتیازهای من")
-    def my_points(message):
+    @bot.message_handler(func=lambda m: m.text == "📦 اشتراک های من")
+    def my_subs(message):
 
-        pts = get_points(message.from_user.id)
+        subs = get_subs(message.from_user.id)
 
-        bot.send_message(
-            message.chat.id,
-            f"امتیاز شما: {pts}"
-        )
+        if not subs:
+            bot.send_message(message.chat.id, "اشتراکی نداری ❌")
+            return
+
+        text = ""
+        for s in subs:
+            text += f"\n{ s[0] } ماهه\nاز { s[1] }\nتا { s[2] }\n"
+
+        bot.send_message(message.chat.id, text)
