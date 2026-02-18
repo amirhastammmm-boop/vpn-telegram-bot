8071ba199fe78260891aaba684582eb34f4e3ef3
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 09:12:41 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 BOT_TOKEN = "8443993110:AAH08NXvjcKty2XBHH13ecD8l5giSCD-ZF4"
 
 ADMIN_ID = "Amnbytry"
-BOT_USERNAME = "PayydarVpn_robot"
+
+NEXT_PAY_LINKS = {
+    "month1": "https://nextpay.org/payment-link-1",
+    "month2": "https://nextpay.org/payment-link-2",
+    "month3": "https://nextpay.org/payment-link-3"
+}
