49fbd546cd8b5b8128861fed9978475aab4a4b03
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 14:56:48 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,48 @@
-from database import add_subscription
+import telebot
+from config import BOT_TOKEN
+from database import create_tables, add_subscription
+from handlers import register_handlers
+from telebot.types import ReplyKeyboardMarkup
 from datetime import datetime, timedelta
 
 
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# ساخت دیتابیس
+create_tables()
+
+
+# ---------------- منوی اصلی ----------------
+def main_menu():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    markup.row("💳 خرید اشتراک", "📦 اشتراک های من")
+    markup.row("🏆 امتیاز های من", "📚 آموزش خرید")
+    markup.row("🛠 پشتیبانی")
+
+    return markup
+
+
+# ---------------- استارت ----------------
+@bot.message_handler(commands=['start'])
+def start(message):
+
+    text = """
+♥️ سلام دوست عزیز
+
+به ربات پینگ خور خوش اومدی
+
+⬇️ لطفا یک گزینه را انتخاب کن
+"""
+
+    bot.send_message(
+        message.chat.id,
+        text,
+        reply_markup=main_menu()
+    )
+
+
+# ---------------- تست ساخت اشتراک ----------------
 @bot.message_handler(commands=['testsub'])
 def test_sub(message):
 
@@ -24,3 +65,11 @@ def test_sub(message):
     add_subscription(data)
 
     bot.send_message(message.chat.id, "✅ اشتراک تستی ساخته شد")
+
+
+# ثبت هندلرها
+register_handlers(bot)
+
+
+print("Bot is running...")
+bot.infinity_polling()
