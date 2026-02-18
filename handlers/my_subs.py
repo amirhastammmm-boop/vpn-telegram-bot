0f9e924605fadd633bf5b2cd3ebdec08e4f75f44
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 19:51:40 +0330

    Update my_subs.py

--- handlers/my_subs.py
+++ handlers/my_subs.py
@@ -1,5 +1,23 @@
+from database import get_user_subscriptions
+
+
 def register_my_sub_handlers(bot):
 
-    @bot.message_handler(func=lambda m: m.text == "📦 اشتراک من")
-    def my_sub(message):
-        bot.send_message(message.chat.id, "شما فعلاً اشتراک فعالی ندارید.")
+    @bot.message_handler(func=lambda m: m.text == "📦 اشتراک های من")
+    def my_subs(message):
+
+        subs = get_user_subscriptions(message.from_user.id)
+
+        if not subs:
+            bot.send_message(message.chat.id, "اشتراکی نداری")
+            return
+
+        text = "📦 اشتراک های شما:\n\n"
+
+        for sub in subs:
+            text += f"📅 شروع: {sub['start']}\n"
+            text += f"⏳ پایان: {sub['end']}\n"
+            text += f"📦 مدت: {sub['months']} ماه\n"
+            text += "-----------------\n"
+
+        bot.send_message(message.chat.id, text)
