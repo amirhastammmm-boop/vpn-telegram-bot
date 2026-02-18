2309b0a0ce48233ed1f3c623f3f9acf8ea3c62fa
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 20:14:38 +0330

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,29 +1,28 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from database import add_user
-
-
-def main_menu():
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-
-    kb.add(KeyboardButton("🛒 خرید اشتراک"))
-    kb.add(KeyboardButton("📦 اشتراک های من"))
-    kb.add(KeyboardButton("🏆 امتیاز های من"))
-
-    return kb
 
 
 def register_start_handlers(bot):
 
-    @bot.message_handler(commands=['start'])
-    def start(message):
+    @bot.message_handler(commands=['ref'])
+    def referral_panel(message):
 
         user_id = message.from_user.id
-        add_user(user_id)
 
-        bot.send_message(
-            message.chat.id,
-            "❤️ سلام دوست عزیز\n"
-            "به ربات خوش اومدی\n\n"
-            "لطفا یک گزینه را انتخاب کن 👇",
-            reply_markup=main_menu()
+        referral_link = f"https://t.me/{bot.get_me().username}?start={user_id}"
+
+        text = f"""
+🎁 لینک دعوت شما:
+
+{referral_link}
+
+📌 با دعوت هر نفر 1 امتیاز میگیری
+"""
+
+        keyboard = InlineKeyboardMarkup()
+
+        keyboard.add(
+            InlineKeyboardButton("📤 اشتراک گذاری لینک", url=f"https://t.me/share/url?url={referral_link}")
         )
+
+        bot.send_message(message.chat.id, text, reply_markup=keyboard)
