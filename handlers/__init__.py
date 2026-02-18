1bd4b04a2b48cd6501527376425f7ff259d89d21
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 18:38:01 +0330

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,12 @@
 from .buy import register_buy_handlers
 from .my_subs import register_my_sub_handlers
 from .points import register_points_handlers
+from .start import register_start_handler
 
 
 def register_handlers(bot):
 
+    register_start_handler(bot)
     register_buy_handlers(bot)
     register_my_sub_handlers(bot)
     register_points_handlers(bot)
