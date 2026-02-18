f1b3af66df3573a006e8e3ca22075a06996d17f2
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 23:53:13 +0330

    Update points.py

--- handlers/points.py
+++ handlers/points.py
@@ -1,5 +1,14 @@
-def add_points(user_id, amount):
-    pass
+from database import get_user, add_points, create_subscription
 
-def get_points(user_id):
-    return 0
+def register_points_handlers(bot):
+
+    @bot.message_handler(func=lambda m: m.text == "🏆 امتیاز های من")
+    def points(message):
+
+        user = get_user(message.from_user.id)
+        pts = user[3]
+
+        bot.send_message(
+            message.chat.id,
+            f"امتیاز شما: {pts}\n\n5 امتیاز = 1 ماه\n9 امتیاز = 2 ماه"
+        )
