3087f6087e61f85dc300a20cacf1c04afd090f4b
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 08:38:16 +0330

    Update buy.py

--- handlers/buy.py
+++ handlers/buy.py
@@ -1,1 +1,8 @@
+from telebot.types import Message
 
+
+def register_buy_handlers(bot):
+
+    @bot.message_handler(func=lambda message: message.text == "🛒 خرید اکانت")
+    def buy_account(message: Message):
+        bot.send_message(message.chat.id, "بخش خرید اکانت به زودی اضافه میشه 🚀")
