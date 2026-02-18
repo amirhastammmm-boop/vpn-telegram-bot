aa3236669ae11c28995d59b2d23c955567a1dd8b
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-19 00:52:12 +0330

    Update buy.py

--- handlers/buy.py
+++ handlers/buy.py
@@ -22,3 +22,12 @@ async def buy_plan(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def register_buy_handlers(app):
     app.add_handler(MessageHandler(filters.Regex("🛒 خرید اشتراک"), buy_handler))
     app.add_handler(MessageHandler(filters.Regex("1 ماهه|2 ماهه|3 ماهه|🔙 بازگشت"), buy_plan))
+async def support(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "در صورت نیاز به پشتیبانی به آیدی زیر پیام دهید:\n@Amnbytry"
+    )
+
+async def tutorial(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "فایل WireGuard و آموزش نصب بعداً اینجا ارسال میشه."
+    )
