cc25b30968b2866a1111fb471b0ddabce0dd070e
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 13:52:45 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,60 +1,70 @@
 import sqlite3
 
 
+# ---------------- ساخت جدول ها ----------------
+
 def create_tables():
 
     conn = sqlite3.connect("bot.db")
     cursor = conn.cursor()
 
-    # جدول اشتراک ها
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS subscriptions (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
-        buy_date TEXT,
-        expire_date TEXT,
-        volume TEXT,
-        private_key TEXT,
-        address TEXT,
-        server_public_key TEXT,
-        endpoint TEXT
-    )
+        CREATE TABLE IF NOT EXISTS subscriptions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            buy_date TEXT,
+            expire_date TEXT,
+            volume TEXT,
+            private_key TEXT,
+            address TEXT,
+            server_public_key TEXT,
+            endpoint TEXT
+        )
     """)
 
     conn.commit()
     conn.close()
 
 
-# ذخیره اشتراک
+# ---------------- افزودن اشتراک ----------------
+
 def add_subscription(data):
 
     conn = sqlite3.connect("bot.db")
     cursor = conn.cursor()
 
     cursor.execute("""
-    INSERT INTO subscriptions
-    (user_id,buy_date,expire_date,volume,private_key,address,server_public_key,endpoint)
-    VALUES (?,?,?,?,?,?,?,?)
+        INSERT INTO subscriptions
+        (user_id, buy_date, expire_date, volume,
+         private_key, address, server_public_key, endpoint)
+
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     """, data)
 
     conn.commit()
     conn.close()
 
 
-# گرفتن همه اشتراک های کاربر
+# ---------------- گرفتن همه اشتراک های کاربر ----------------
+
 def get_user_subscriptions(user_id):
 
     conn = sqlite3.connect("bot.db")
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM subscriptions WHERE user_id=?", (user_id,))
+    cursor.execute(
+        "SELECT * FROM subscriptions WHERE user_id=?",
+        (user_id,)
+    )
+
     subs = cursor.fetchall()
 
     conn.close()
     return subs
 
 
-# گرفتن یک اشتراک خاص
+# ---------------- گرفتن یک اشتراک خاص ----------------
+
 def get_subscription(user_id, sub_id):
 
     conn = sqlite3.connect("bot.db")
