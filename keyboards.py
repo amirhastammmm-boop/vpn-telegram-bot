16343a26fc314ddba4343afe3ddf6e4940c52631
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-18 13:55:02 +0330

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -2,12 +2,13 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 def main_menu():
+
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
 
     btn1 = KeyboardButton("🛒 خرید اشتراک")
     btn2 = KeyboardButton("📦 اشتراک های من")
 
-    btn3 = KeyboardButton("🏆 امتیاز من")
+    btn3 = KeyboardButton("🏆 امتیاز های من")
     btn4 = KeyboardButton("📚 آموزش خرید")
 
     btn5 = KeyboardButton("🎧 پشتیبانی")
