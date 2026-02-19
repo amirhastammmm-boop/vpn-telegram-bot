611ebabc819255dfd7f1b462a6e01b6cddf5a356
amirhastammmm-boop <amirhastammmm@gmail.com>
2026-02-19 13:59:55 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8443993110:AAH08NXvjcKty2XBHH13ecD8l5giSCD-ZF4")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/vpnbot_db")
 
-SUPPORT_USERNAME = "Amnbytry@"
-SECRET_KEY = "CHANGE_THIS_SECRET_KEY"
+SUPPORT_USERNAME = "@Amnbytry"
+SECRET_KEY = os.getenv("SECRET_KEY", "CHANGE_THIS_SECRET_KEY")
